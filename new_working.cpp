0e8f180ec95a0971f5a4a2ddc143bf47fa23d9a0
abs-d07 <93074613+abs-d07@users.noreply.github.com>
2023-02-14 12:17:27 +0530

    Create new_working.cpp

---
+++ new_working.cpp
@@ -0,0 +1,10 @@
+//File: new_working.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout<<"New working CPP file !!!\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
